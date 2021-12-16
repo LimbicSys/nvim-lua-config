6e90f97c86683242833203d83070bafe5f4f7778
LimbicSys <yu443940816@live.com>
2021-12-16 15:13:03 +0800

    add range formatting

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -5,3 +5,5 @@ vim.cmd [[ augroup formatOnSave
 
 vim.cmd([[ command! ToggleFormat lua require"format-wrapper".toggle_format_on_save() ]])
 vim.cmd([[ command! FormatBuffer lua require"format-wrapper".formatting() ]])
+
+vim.cmd [[command! -nargs=? -range=% -bar RangeFormat lua vim.lsp.buf.range_formatting()]]
