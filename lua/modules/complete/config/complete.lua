a7f35ee50949f3c865f763b815a8dfa98072cbd5
LimbicSys <yu443940816@live.com>
2022-07-26 19:15:49 +0800

    disable insert when select a completion item

--- lua/modules/complete/config/complete.lua
+++ lua/modules/complete/config/complete.lua
@@ -17,8 +17,8 @@ cmp.setup({
   },
   -- You can set mapping if you want.
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
