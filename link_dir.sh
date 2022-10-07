03b3dfb5cc7da4e770d161110ce9b130f3a689f5
LimbicSys <yu443940816@live.com>
2022-10-07 15:20:13 +0800

    update

--- link_dir.sh
+++ link_dir.sh
@@ -2,3 +2,21 @@
 
 currentPath=`pwd`
 ln -s $currentPath ~/.config/nvim
+
+files=("tasks.ini")
+link_targets_prefix=("${HOME}/.vim")
+
+for ((i=0; i<${#files[@]}; i++))
+do
+    link_target=$HOME/${link_targets[$i]}
+
+    file=$currentPath/${files[$i]}
+    prefix=${link_targets_prefix[$i]}
+    link_target=$prefix/${files[$i]}
+
+    # echo $link_target $file
+    if [ ! -d $prefix ]; then
+        mkdir -p $prefix
+    fi
+    ln -s -f $file $link_target
+done
