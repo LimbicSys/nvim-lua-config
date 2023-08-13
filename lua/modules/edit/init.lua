37d5bfed26468897800dd2e31b93c64039b4fd39
LimbicSys <yu443940816@live.com>
2023-08-13 11:59:27 +0800

    use flash.nvim

--- lua/modules/edit/init.lua
+++ lua/modules/edit/init.lua
@@ -13,11 +13,22 @@ return {
   },
 
   {
-    "ggandor/leap.nvim",
+    "folke/flash.nvim",
     event = "VeryLazy",
-    config = function()
-      require("modules.edit.config.leap")
-    end,
+    ---@type Flash.Config
+    opts = {
+      char = {
+        enabled = true,
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
 
   { "tpope/vim-surround", event = "VeryLazy" },
